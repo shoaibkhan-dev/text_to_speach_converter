6dcc5baaf616f62ddec3733f3a8dc5b734b72840
Shoaib khan <shoaibmangal@yahoo.com>
2025-06-24 20:45:48 +0500

    add fwork

--- app.js
+++ app.js
@@ -28,3 +28,10 @@ let speech = new SpeechSynthesisUtterance();
       speech.text = document.querySelector("textarea").value;
       window.speechSynthesis.speak(speech);
     });
+
+
+    function greet (){
+      console.log("hello")
+    }
+
+    greet()
